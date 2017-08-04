fcb6a267d203a7298fdbb74131c19189a4c5d0f1
wz2cool <wz2cool@live.cn>
2017-08-04 10:17:32 +0800

    add

---
+++ src/main/java/Application.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: Frank
+ * \* Date: 8/4/2017
+ * \* Time: 10:14 AM
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
